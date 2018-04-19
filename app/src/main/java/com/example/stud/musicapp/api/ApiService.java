7a610d995fe6abeecf7aafdb91ba4173fd2bfbf8
dariabielenda <daria.bielenda@onet.pl>
2018-04-19 17:57:56 +0200

    API

---
+++ app/src/main/java/com/example/stud/musicapp/api/ApiService.java
@@ -0,0 +1,17 @@
+package com.example.stud.musicapp.api;
+
+
+import retrofit2.Retrofit;
+
+public class ApiService {
+
+    public static ApiClient getService() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl( "http://www.theaudiodb.com/api/v1/json/{APIKEY}/" )
+                .build();
+        return retrofit.create(ApiClient. class );
+    }
+
+
+
+}
