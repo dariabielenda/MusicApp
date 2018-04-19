989633605b03cf36c7ed3ccde69c6ca4000eb916
dariabielenda <daria.bielenda@onet.pl>
2018-04-19 19:17:07 +0200

    tytul

--- app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
@@ -1,19 +1,54 @@
 package com.example.stud.musicapp.topsongs;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.stud.musicapp.R;
+import com.example.stud.musicapp.api.ApiService;
+import com.example.stud.musicapp.api.TrendingList;
+import com.google.gson.Gson;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TopSongsActivity extends AppCompatActivity {
 
+    RecyclerView rvList;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_songs);
 
+        rvList = findViewById(R.id.rvList);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final Call<TrendingList> trendingListCall = ApiService. getService ().getTrendingList( "us" ,
+                "itunes" , "singles" );
+        trendingListCall.enqueue( new Callback<TrendingList>() {
+            @Override
+            public void onResponse(@NonNull Call<TrendingList> call, @NonNull
+                    Response<TrendingList> response) {
+                TrendingList trendingList = response.body();
+                Toast.makeText(TopSongsActivity.this, "Elementow: " + trendingList.trending.size(),
+                        Toast.LENGTH_SHORT).show();
+                Log. d ( "TAG" , new Gson().toJson(trendingList));
+            }
+            @Override
+            public void onFailure( @NonNull Call<TrendingList> call, Throwable t) {
+                Toast. makeText (TopSongsActivity. this , "Blad pobierania danych: " +
+                        t.getLocalizedMessage(), Toast. LENGTH_SHORT ).show();
+
+
+            }
+        });
     }
     @Override
     public boolean onSupportNavigateUp() {
